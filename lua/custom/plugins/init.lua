93aad2dbf06165cfb4a5e9e6fc9254ca08630cc1
Terry <78312274+TerryEmeigh@users.noreply.github.com>
2025-06-10 16:20:51 -0400

    complete transparency

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -26,6 +26,74 @@ return {
         keywordStyle = { italic = false },
         overrides = function()
           return {
+            -- Main editing areas
+            Normal = { bg = 'none' },
+            NormalNC = { bg = 'none' },
+            NormalFloat = { bg = 'none' },
+            FloatBorder = { bg = 'none' },
+            FloatTitle = { bg = 'none' },
+
+            -- Popups and menus
+            Pmenu = { bg = 'none' },
+            PmenuSel = { bg = 'none' },
+            PmenuSbar = { bg = 'none' },
+            PmenuThumb = { bg = 'none' },
+
+            -- Status and command line
+            StatusLine = { bg = 'none' },
+            StatusLineNC = { bg = 'none' },
+            WinBar = { bg = 'none' },
+            WinBarNC = { bg = 'none' },
+            MsgArea = { bg = 'none' },
+            Cmdline = { bg = 'none' },
+            NoiceCmdlinePopup = { bg = 'none' },
+            NoiceCmdlinePopupBorder = { bg = 'none' },
+
+            -- Tabs and splits
+            TabLine = { bg = 'none' },
+            TabLineSel = { bg = 'none' },
+            TabLineFill = { bg = 'none' },
+            VertSplit = { bg = 'none' },
+            WinSeparator = { bg = 'none' },
+
+            -- Telescope and floating UIs
+            TelescopeNormal = { bg = 'none' },
+            TelescopeBorder = { bg = 'none' },
+            TelescopePromptNormal = { bg = 'none' },
+            TelescopePromptBorder = { bg = 'none' },
+            TelescopePreviewNormal = { bg = 'none' },
+            TelescopePreviewBorder = { bg = 'none' },
+            TelescopeResultsNormal = { bg = 'none' },
+            TelescopeResultsBorder = { bg = 'none' },
+
+            -- LSP floating windows
+            LspInfoBorder = { bg = 'none' },
+            LspFloatWinNormal = { bg = 'none' },
+
+            -- Treesitter context
+            TreesitterContext = { bg = 'none' },
+
+            -- Diagnostic sings (errors, warnings, etc.)
+            DiagnosticSignError = { bg = 'none' },
+            DiagnosticSignWarn = { bg = 'none' },
+            DiagnosticSignInfo = { bg = 'none' },
+            DiagnosticSignHint = { bg = 'none' },
+
+            -- Git signs (for gitsigns.nvim)
+            GitSignsAdd = { bg = 'none' },
+            GitSignsChange = { bg = 'none' },
+            GitSignsDelete = { bg = 'none' },
+
+            -- Misc
+            SignColumn = { bg = 'none' },
+            FoldColumn = { bg = 'none' },
+            LineNr = { bg = 'none' },
+            CursorLineNr = { bg = 'none' },
+            EndOfBuffer = { bg = 'none' },
+            ColorColumn = { bg = 'none' },
+            CursorLine = { bg = 'none' },
+
+            -- Language highlights
             ['@variable.builtin'] = { italic = false },
           }
         end,
